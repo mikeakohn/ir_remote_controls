1d1d74b9a7619b0a16a6cde499e9838c791eb5f1
Michael Kohn <mike@mikekohn.net>
2017-07-17 19:16:03 -0500

    Separate out IR sending in its own file.

--- universal_remote/universal_remote.asm
+++ universal_remote/universal_remote.asm
@@ -78,6 +78,8 @@ start:
   mov.b #0x00, &P1OUT
   mov.b #0x06, &P1SEL
   mov.b #0x06, &P1SEL2
+  mov.b #0x06, &P2DIR
+  mov.b #0x06, &P2OUT
 
   ;; Set MCLK to 4 MHz with DCO
   mov.b #DCO_2, &DCOCTL
@@ -99,6 +101,9 @@ start:
   mov.w #CCIE, &TBCCTL0
   mov.w #0, &TBCCTL1
 
+  ;; Enable interrupts.
+  eint
+
   call #calibrate_baud_rate
 
   ;; Change interrupt count on Timer A
@@ -107,52 +112,14 @@ start:
   ;; Disable Timer B
   mov.w #0, &TBCCTL0
 
+  ;; Turn of LED's until stuff happens
+  mov.b #0x00, &P2OUT
+
   mov.b #0, r10
 
-  ;; Enable interrupts.
-  eint
-
 main:
-  bit.b #0x40, &P1IN
-  jnz main
-
-;  mov.w #240, r15
-;main_send_delay:
-;  call #wait_500ms
-;  dec r15
-;  jnz main_send_delay
-
-  ;call #set_dco_4mhz
-
-  ;; Turn on button debug LED.
-  bis.b #0x10, &P1OUT
-
-  ;; Debounce.
-wait_button:
-  bit.b #0x40, &P1IN
-  jz wait_button
-
-  ;; Turn off button debug LED.
-  bic.b #0x10, &P1OUT
-
-  ;; Set command to 7
-  mov.w #KEY_1, r4
-  call #send_command
-  call #delay
-
-  ;; Set command to 8
-  mov.w #KEY_6, r4
-  call #send_command
-  call #delay
-
-  ;; Set command to 6
-  mov.w #KEY_5, r4 
-  call #send_command
-  call #delay
-
-  ;call #set_dco_slow
-
   jmp main
+
 
 delay:
   mov.w #0xffff, r15
@@ -161,123 +128,8 @@ delay_loop:
   jnz delay_loop
   ret
 
-send_command:
-  ;; Turn on send debug LED.
-  bis.b #0x20, &P1OUT
-
-  ;; Send header on.
-  call #send_header_on
-
-  ;; Send header off.
-  clr.w r9
-wait_header_off:
-  cmp.w #HEADER_OFF, r9
-  jne wait_header_off
-
-  ;; for (r5 = 0; r5 < 33; r5++)
-  mov.w #16, r5
-next_bit:
-
-  ;; Turn bit on.
-  clr.w r9
-  mov.b #1, r10
-wait_bit_on:
-  cmp.w #DIVIDER, r9
-  jne wait_bit_on
-  mov.b #0, r10
-  bic.b #1, &P1OUT
-
-  ;; Compute length of bit.
-  mov.w #ZERO, r11
-  bit.w #0x8000, r4
-  jz is_zero
-  mov.w #ONE, r11
-is_zero:
-
-  ;; Turn bit off
-  clr.w r9
-wait_bit_off:
-  cmp.w r11, r9
-  jne wait_bit_off
-
-  ;; Shift bit and increment count and possibly command pointer.
-  add.w r4, r4
-
-  ;; Next.
-  dec.w r5
-  jnz next_bit
-
-  ;; Send ptrail
-  call #send_ptrail
-
-  ;; Wait 30ms and send half header and ptrail
-  call #wait_30ms
-  call #send_header_on
-  call #wait_2_25ms
-  call #send_ptrail
-
-  ;; Wait 90ms and send half header and ptrail
-  call #wait_30ms
-  call #wait_30ms
-  call #wait_30ms
-  call #send_header_on
-  call #wait_2_25ms
-  call #send_ptrail
-
-  ;; Turn off send debug LED.
-  bic.b #0x20, &P1OUT
-
-  ;; Gap at end.
-  clr.w r9
-wait_gap:
-  cmp.w #GAP_LENGTH, r9
-  jne wait_gap
-
-  ;; Done. Repeat.
-  ret
-
-send_header_on:
-  clr.w r9
-  mov.b #1, r10
-wait_header_on:
-  cmp.w #HEADER_ON, r9
-  jne wait_header_on
-  mov.b #0, r10
-  bic.b #1, &P1OUT
-  ret
-
-send_ptrail:
-  clr.w r9
-  mov.b #1, r10
-wait_ptrail:
-  cmp.w #DIVIDER, r9
-  jne wait_ptrail
-  mov.b #0, r10
-  bic.b #1, &P1OUT
-  ret
-
-wait_30ms:
-  clr.w r9
-wait_30ms_loop:
-  cmp.w #DELAY_30MS, r9
-  jne wait_30ms_loop
-  ret
-
-wait_500ms:
-  clr.w r9
-wait_500ms_loop:
-  cmp.w #DELAY_500MS, r9
-  jne wait_500ms_loop
-  ret
-
-wait_2_25ms:
-  clr.w r9
-wait_2_25ms_loop:
-  cmp.w #ZERO, r9
-  jne wait_2_25ms_loop
-  ret
-
 .include "calibrate.inc"
+.include "send_ir.inc"
 
 // DCO based interrupt
 timer_interrupt_a:
